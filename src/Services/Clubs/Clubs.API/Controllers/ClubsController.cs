f89975bab1a12a386d860c6dbabb5665b2c52bcc
Sean <srafferty89@gmail.com>
2020-05-24 19:44:00 +0100

    more initial settp logic including

---
+++ src/Services/Clubs/Clubs.API/Controllers/ClubsController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Clubs.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public abstract class ClubsController : ControllerBase
+    {
+        public ClubsController()
+        {
+            
+        }
+    }
+}
---
+++ src/Services/Clubs/Clubs.Domain/Common/AuditableEntity.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Clubs.Domain.Common
+{
+    public abstract class AuditableEntity
+    {
+        public string CreatedBy { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public string LastModifiedBy { get; set; }
+
+        public DateTime? LastModified { get; set; }
+    }
+}
