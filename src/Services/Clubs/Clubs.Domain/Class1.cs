9077b43a83de1ca2d6e0b5e7ca97a80d7531fe49
Sean <srafferty89@gmail.com>
2020-05-24 19:10:45 +0100

    First projects added for API/backend

---
+++ src/Services/Clubs/Clubs.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Clubs.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/Services/Clubs/Clubs.Infrastructure/ClubsContext.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace Clubs.Infrastructure
+{
+    public class ClubsContext
+    {
+    }
+}
