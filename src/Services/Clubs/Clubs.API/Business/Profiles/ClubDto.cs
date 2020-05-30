61548602397f4f2918aa9d9edb3ca4499c385b92
Sean <srafferty89@gmail.com>
2020-05-30 17:17:59 +0100

    Added ApiController and Updates clubs

--- src/Services/Clubs/Clubs.API/Business/Profiles/ClubDto.cs
+++ src/Services/Clubs/Clubs.API/Business/Profiles/ClubDto.cs
@@ -1,9 +1,15 @@
 
 
+using System;
+
 namespace Clubs.API.Managers.Profiles
 {
     public class ClubDto
     {
-        
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public bool Active { get; set; } = true;
     }
 }
--- src/Services/Clubs/Clubs.API/Controllers/ClubsController.cs
+++ src/Services/Clubs/Clubs.API/Controllers/ClubsController.cs
@@ -1,11 +1,15 @@
 using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Club.API.Controllers;
+using Clubs.API.Club.Queries;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Clubs.API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public abstract class ClubsController : ControllerBase
+    public abstract class ClubsController : ApiController
     {
         public ClubsController()
         {
@@ -13,9 +17,14 @@ namespace Clubs.API.Controllers
         }
 
         [HttpGet]
-        public IActionResult Get()
+        public async Task<IActionResult> Get()
         {
-            throw new NotImplementedException();
+            var result = await Mediator.Send(new GetClubsQuery());
+
+            if (result.Any())
+                return Ok(result);
+
+            return StatusCode(222, "No Clubs Found");
         }
 
         [HttpGet("{id}")]
