a212f9d88ff8b5dca61c26ab7932d811f155a5ef
Sean <srafferty89@gmail.com>
2020-06-09 18:54:59 +0100

    new members logic

---
+++ src/Services/Clubs/Clubs.API/Business/Member/GetAllMembersQuery.cs
@@ -0,0 +1,44 @@
+using Clubs.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Linq;
+using MediatR;
+using System;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Clubs.API.Managers.Profiles;
+using Clubs.Infrastructure;
+
+namespace Clubs.API.Club.Queries
+{
+    //Following this concept: https://github.com/jasontaylordev/CleanArchitecture/blob/a731538e35d5ff21cd2ba937bef60a41993970dd/src/Application/TodoLists/Queries/GetTodos/GetTodosQuery.cs
+
+    public class GetAllMembersQuery : IRequest<IEnumerable<MemberDto>>
+    {
+    }
+
+    public class GetAllMembersQueryHandler : IRequestHandler<GetAllMembersQuery, IEnumerable<MemberDto>>
+    {
+        private readonly ClubsContext _Context;
+        private readonly IMapper _Mapper;
+
+        public GetAllMembersQueryHandler(ClubsContext context, IMapper mapper)
+        {
+            _Context = context;
+            _Mapper = mapper;
+        }
+
+        public async Task<IEnumerable<MemberDto>> Handle(GetAllMembersQuery request, CancellationToken cancellationToken)
+        {
+            return await _Context.Members
+                .ProjectTo<MemberDto>(_Mapper.ConfigurationProvider)
+                .AsNoTracking()
+                .ToListAsync(cancellationToken);
+        }
+    }
+}
+
+
+
--- src/Services/Clubs/Clubs.API/Controllers/MemberController.cs
+++ src/Services/Clubs/Clubs.API/Controllers/MemberController.cs
@@ -6,24 +6,45 @@ using System.Threading.Tasks;
 using Club.API.Controllers;
 using Clubs.API.Club.Queries;
 using Clubs.API.Managers.Profiles;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Utilities;
 
 namespace Clubs.API.Controllers
 {
     public class MemberController : ApiController
     {
-        private readonly ILogger<MemberController> _logger;
+        private readonly ILogger<MemberController> _Logger;
         public MemberController(ILogger<MemberController> logger)
         {
-            _logger = logger;
+            _Logger = logger;
         }
 
 
-        // [HttpGet]
-        // public async Task<IEnumerable<PlayerDto>> Get()
-        // {
-        //     return await Mediator.Send(new GetPlayersQuery());
-        // }
+        [HttpGet]
+        public async Task<IEnumerable<MatchDto>> Get()
+        {
+            return await Mediator.Send(new GetMatchesQuery());
+        }
+
+        /// <summary>
+        /// Query a single Club and its details
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id}", Name="GetById")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            _Logger.LogInformation($"method: {HelperMethods.GetCallerMemberName()}");
+            // var result = await Mediator.Send(new GetClubQuery() {Id = id});
+
+            // if (result != null)
+            //     return Ok(result);
+
+            return StatusCode(204, "No Record Found");
+        }
     }
 }
