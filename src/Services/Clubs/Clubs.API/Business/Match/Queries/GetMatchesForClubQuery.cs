96d4a1e79074033cc13137ca292ab7fd2b2c87c1
Sean <srafferty89@gmail.com>
2020-06-13 12:18:03 +0100

    brand new query made!

---
+++ src/Services/Clubs/Clubs.API/Business/Match/Queries/GetMatchesForClubQuery.cs
@@ -0,0 +1,43 @@
+using Clubs.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Linq;
+using MediatR;
+using System;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Clubs.API.Managers.Profiles;
+using Clubs.Infrastructure;
+
+namespace Clubs.API.Club.Queries
+{
+    //Following this concept: https://github.com/jasontaylordev/CleanArchitecture/blob/a731538e35d5ff21cd2ba937bef60a41993970dd/src/Application/TodoLists/Queries/GetTodos/GetTodosQuery.cs
+
+    public class GetMatchesForClubQuery : IRequest<IEnumerable<MatchDto>>
+    {
+        public Guid ClubId { get; set; }
+    }
+
+    public class GetMatchesForClubQueryHandler : IRequestHandler<GetMatchesForClubQuery, IEnumerable<MatchDto>>
+    {
+        private readonly ClubsContext _Context;
+        private readonly IMapper _Mapper;
+
+        public GetMatchesForClubQueryHandler(ClubsContext context, IMapper mapper)
+        {
+            _Context = context;
+            _Mapper = mapper;
+        }
+
+        public async Task<IEnumerable<MatchDto>> Handle(GetMatchesForClubQuery request, CancellationToken cancellationToken)
+        {
+            return await _Context.Matches
+                .Where(m => m.ClubId == request.ClubId)
+                .ProjectTo<MatchDto>(_Mapper.ConfigurationProvider)
+                .AsNoTracking()
+                .ToListAsync(cancellationToken);
+        }
+    }
+}
