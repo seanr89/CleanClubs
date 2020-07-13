31c743c1d31100a4b405df9c92776164cea6bc7d
seanr89 <srafferty89@gmail.com>
2020-07-13 15:37:26 +0100

    New Application proj created

---
+++ src/Services/Clubs/Clubs.Application/DependencyInjection.cs
@@ -0,0 +1,13 @@
+
+
+namespace Clubs.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+        }
+    }
+}
