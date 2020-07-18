b060426f2308b7605c7326b8a3d4886703400465
Sean <srafferty89@gmail.com>
2020-07-18 18:00:28 +0100

    moving clubs request logic

--- src/Services/Clubs/Clubs.Application/Requests/Club/Commands/UpdateClubCommand.cs
+++ src/Services/Clubs/Clubs.Application/Requests/Club/Commands/UpdateClubCommand.cs
@@ -8,10 +8,10 @@ using MediatR;
 using System;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
-using Clubs.API.Managers.Profiles;
 using Clubs.Infrastructure;
+using Clubs.Application.Profiles.DTOs.Clubs;
 
-namespace Clubs.API.Club.Commands
+namespace Clubs.Application.Requests.Club.Commands
 {
     //Following this concept: https://github.com/jasontaylordev/CleanArchitecture/blob/a731538e35d5ff21cd2ba937bef60a41993970dd/src/Application/TodoLists/Queries/GetTodos/GetTodosQuery.cs
 
