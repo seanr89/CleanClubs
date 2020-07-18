a5dda7c7cafb83899997cb09140e55261b7fdfda
Sean <srafferty89@gmail.com>
2020-07-18 18:53:41 +0100

    new logic with support from sendgrid

--- src/Services/Clubs/Clubs.Emails/EmailHandler.cs
+++ src/Services/Clubs/Clubs.Emails/EmailHandler.cs
@@ -1,19 +1,43 @@
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using Microsoft.Extensions.Logging;
 
 namespace Clubs.Emails
 {
     public class EmailHandler
     {
-        public EmailHandler()
+        private readonly ILogger<EmailHandler> _Logger;
+        public EmailHandler(ILogger<EmailHandler> logger)
         {
-            
+            _Logger = logger;
         }
 
         public async Task<bool> SendEmail(string email, string messageContent, string subject)
         {
-            throw new NotImplementedException();
+             {
+                //Send email with report to users.
+                var client = new SendGridClient("apiKey");
+                var from = new EmailAddress("srafferty89@gmail.com", "Sean");
+                var to = new EmailAddress(email);
+                var subject = "Match Invitation";
+                var plainTextContent = $@"Message:";
+                
+                var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, messageContent);
+                
+                var response = await client.SendEmailAsync(msg);
+
+                if(response.StatusCode == HttpStatusCode.BadRequest)
+                    _Logger.LogError($"Email Manager error triggered in the method: {HelperMethods.GetCallerMemberName()} while sending an email to {testData.Email} with exception: {response}");
+
+                return (response.StatusCode == HttpStatusCode.OK || response.StatusCode == HttpStatusCode.Accepted); ;
+            }
+            catch
+            {
+                return false;
+            }
         }
     }
 }
