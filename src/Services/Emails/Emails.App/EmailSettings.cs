75ec0c97996b8458468ba13d84d824253163df81
Sean <srafferty89@gmail.com>
2020-07-24 15:57:09 +0100

    more settings content added

--- src/Services/Emails/Emails.App/EmailSettings.cs
+++ src/Services/Emails/Emails.App/EmailSettings.cs
@@ -5,5 +5,6 @@ namespace Emails.App
     public class EmailSettings
     {
         public string AccessKey { get; set; }
+        public string SenderEmail { get; set; } = "srafferty89@gmail.com"
     }
 }
