8cdf1ad28fb3452e515a8634bb4ec374ef1bcc26
seanr89 <srafferty89@gmail.com>
2020-07-25 09:45:32 +0100

    Updated app settings logic

---
+++ src/Services/Emails/Emails.Api/Extensions/ServiceExtensions.cs
@@ -0,0 +1,19 @@
+
+
+namespace Emails.Api.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy($"{configuration["Cors:PolicyName"]}",
+                    builder => builder.WithOrigins($"{configuration["Cors:Origin"]}")
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+            });
+            //builder => builder.AllowAnyOrigin()
+        }
+    }
+}
