0320335ec4264749fa2b57f2f4e844ae4e4e4f2f
seanr89 <srafferty89@gmail.com>
2020-07-26 14:01:21 +0100

    Saving credentials for DB

--- src/Services/Clubs/Clubs.API/Startup.cs
+++ src/Services/Clubs/Clubs.API/Startup.cs
@@ -5,8 +5,6 @@ using System.Threading.Tasks;
 using Clubs.API.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
--- src/Services/Clubs/Clubs.Infrastructure/README.md
+++ src/Services/Clubs/Clubs.Infrastructure/README.md
@@ -1,3 +1,9 @@
-
 # UseFul DB Migration Command
+
 cmd is `dotnet-ef migrations add <ModelName> --startup-project ..\Clubs.API\Clubs.API.csproj --context ClubsContext`
+
+## DB Creds - Dev
+Server:
+DB:
+username: `clubaccessuser`
+password: `PY7ytvF$!1Tq`
