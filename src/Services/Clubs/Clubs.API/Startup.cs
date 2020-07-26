3e0337d8d92bb1a5c8ac8e5cd24d74b6a1f6a2ee
seanr89 <srafferty89@gmail.com>
2020-07-26 20:10:45 +0100

    lets try the settings again!

--- src/Services/Clubs/Clubs.API/Startup.cs
+++ src/Services/Clubs/Clubs.API/Startup.cs
@@ -18,9 +18,16 @@ namespace Clubs.API
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, Microsoft.AspNetCore.Hosting.IHostingEnvironment env)
         {
-            Configuration = configuration;
+            //Configuration = configuration;
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
         }
 
         private string MyAllowSpecificOrigins = "CorsPolicy";
