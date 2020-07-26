2dc7694d20a83935e0a22724166e866e7278b9f3
seanr89 <srafferty89@gmail.com>
2020-07-26 19:43:35 +0100

    new homecontroller logic

--- src/Services/Clubs/Clubs.API/Controllers/HomeController.cs
+++ src/Services/Clubs/Clubs.API/Controllers/HomeController.cs
@@ -1,22 +1,25 @@
 
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace Clubs.API.Controllers
 {
-
+    [Route("api/[controller]/[action]")]
     public class HomeController : ControllerBase
     {
+        private readonly IConfiguration _Configuration;
         private readonly ILogger<HomeController> _Logger;
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
         {
             _Logger = logger;
+            _Configuration = configuration;
         }
 
         [HttpGet]
-        // public async Task<IActionResult> GetDBCon()
-        // {
-
-        // }
+        public IActionResult GetDBCon()
+        {
+            return Ok(_Configuration.GetValue<string>("ConnectionSettings:Default"));
+        }
     }
 }
