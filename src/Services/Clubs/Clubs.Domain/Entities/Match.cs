13a9ecc8a7cf218593414d22575877f995aaeaf3
seanr89 <srafferty89@gmail.com>
2020-07-27 21:18:39 +0100

    tweaked the match record for save testing

--- src/Services/Clubs/Clubs.Domain/Entities/Match.cs
+++ src/Services/Clubs/Clubs.Domain/Entities/Match.cs
@@ -13,14 +13,14 @@ namespace Clubs.Domain.Entities
         [Key]
         public Guid Id { get; set; }
         public Guid ClubId { get; set; }
-        public Club Club { get; set; }
+        //public Club Club { get; set; }
         public DateTime Date { get; set; }
         public MatchStatus Status { get; set; }
         public ICollection<Team> Teams { get; set; }
         public ICollection<Invite> Invites { get; set; }
         public bool InvitesSent { get; set; } = false;
         public GeneratorType Generator { get; set; } = GeneratorType.None;
-        public string Location {get; set;}
-        
+        public string Location { get; set; }
+
     }
 }
