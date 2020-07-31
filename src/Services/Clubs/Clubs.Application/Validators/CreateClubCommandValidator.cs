7b459118004917ed7f20dff9ba77ab16efcbc337
Sean <srafferty89@gmail.com>
2020-07-31 22:50:34 +0100

    new logic and files been moved for command queries

---
+++ src/Services/Clubs/Clubs.Application/Validators/CreateClubCommandValidator.cs
@@ -0,0 +1,9 @@
+//using FluentValidation; 
+
+using Clubs.Application.Requests.Club.Commands;
+
+namespace Clubs.Application.Validators
+{
+    // public class CreateClubCommandValidator : AbstractValidator<CreateClubCommand>
+    // {}
+}
