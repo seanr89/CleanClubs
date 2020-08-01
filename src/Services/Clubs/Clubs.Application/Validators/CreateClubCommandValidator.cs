aa6e216fe92e1de681fd017ada55da4ba2adb828
Sean <srafferty89@gmail.com>
2020-08-01 12:36:01 +0100

    new fluentvalidator for club creation

--- src/Services/Clubs/Clubs.Application/Validators/CreateClubCommandValidator.cs
+++ src/Services/Clubs/Clubs.Application/Validators/CreateClubCommandValidator.cs
@@ -1,9 +1,9 @@
-//using FluentValidation; 
+using FluentValidation; 
 
 using Clubs.Application.Requests.Club.Commands;
 
 namespace Clubs.Application.Validators
 {
-    // public class CreateClubCommandValidator : AbstractValidator<CreateClubCommand>
-    // {}
+    public class CreateClubCommandValidator : AbstractValidator<CreateClubCommand>
+    {}
 }
