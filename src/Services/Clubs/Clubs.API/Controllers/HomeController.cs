ca94c9744f6b60dcbe5d1e147092a49f1cd3fd8e
seanr89 <srafferty89@gmail.com>
2020-08-22 18:04:07 +0100

    Tagging in the controller logic

--- src/Services/Clubs/Clubs.API/Controllers/HomeController.cs
+++ src/Services/Clubs/Clubs.API/Controllers/HomeController.cs
@@ -6,30 +6,35 @@ using Microsoft.Extensions.Logging;
 
 namespace Clubs.API.Controllers
 {
-    [Route("api/[controller]/[action]")]
+    //[Route("api/[controller]/[action]")]
     public class HomeController : ControllerBase
     {
-        private readonly IConfiguration _Configuration;
-        private readonly ILogger<HomeController> _Logger;
-        private readonly IWebHostEnvironment _HostingEnv;
-        public HomeController(ILogger<HomeController> logger, IConfiguration configuration,
-            IWebHostEnvironment hostingEnvironment)
+        // private readonly IConfiguration _Configuration;
+        // private readonly ILogger<HomeController> _Logger;
+        // private readonly IWebHostEnvironment _HostingEnv;
+        public HomeController()
         {
-            _Logger = logger;
-            _Configuration = configuration;
-            _HostingEnv = hostingEnvironment;
+            //_Logger = logger;
+            // _Configuration = configuration;
+            // _HostingEnv = hostingEnvironment;
         }
 
-        [HttpGet]
-        public IActionResult GetDBCon()
+        // GET: /<controller>/
+        public IActionResult Index()
         {
-            return Ok(_Configuration.GetValue<string>("ConnectionSettings:Default"));
+            return new RedirectResult("~/swagger");
         }
 
-        [HttpGet]
-        public IActionResult GetEnv()
-        {
-            return Ok(_HostingEnv.EnvironmentName);
-        }
+        // [HttpGet]
+        // public IActionResult GetDBCon()
+        // {
+        //     return Ok(_Configuration.GetValue<string>("ConnectionSettings:Default"));
+        // }
+
+        // [HttpGet]
+        // public IActionResult GetEnv()
+        // {
+        //     return Ok(_HostingEnv.EnvironmentName);
+        // }
     }
 }
--- src/Services/Emails/Emails.Api/Extensions/ServiceExtensions.cs
+++ src/Services/Emails/Emails.Api/Extensions/ServiceExtensions.cs
@@ -7,16 +7,16 @@ namespace Emails.Api.Extensions
 {
     public static class ServiceExtensions
     {
-        public static void ConfigureCors(this IServiceCollection services, IConfiguration configuration)
-        {
-            services.AddCors(options =>
-            {
-                options.AddPolicy($"{configuration["Cors:PolicyName"]}",
-                    builder => builder.WithOrigins($"{configuration["Cors:Origin"]}")
-                    .AllowAnyMethod()
-                    .AllowAnyHeader());
-            });
-            //builder => builder.AllowAnyOrigin()
-        }
+        // public static void ConfigureCors(this IServiceCollection services, IConfiguration configuration)
+        // {
+        //     services.AddCors(options =>
+        //     {
+        //         options.AddPolicy($"{configuration["Cors:PolicyName"]}",
+        //             builder => builder.WithOrigins($"{configuration["Cors:Origin"]}")
+        //             .AllowAnyMethod()
+        //             .AllowAnyHeader());
+        //     });
+        //     //builder => builder.AllowAnyOrigin()
+        // }
     }
 }
