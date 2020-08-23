058ea52c91f6e758c5dbfb3ffb33b77f8bbabe04
seanr89 <srafferty89@gmail.com>
2020-08-23 12:03:57 +0100

    Reconfiguring background service

--- src/Services/Emails/Emails.App/InvitationReceiverService.cs
+++ src/Services/Emails/Emails.App/InvitationReceiverService.cs
@@ -15,32 +15,33 @@ namespace Emails.App
         private readonly ILogger<InvitationReceieverService> _Logger;
         private readonly EmailHandler _EmailHandler;
 
-        public InvitationReceieverService(ISubscriptionClient subscriptionClient)
+        public InvitationReceieverService(ISubscriptionClient subscriptionClient
+            , EmailHandler emailHandler, ILogger<InvitationReceieverService> logger)
         {
-            //_EmailHandler = emailHandler;
-            //_SubscriptionClient = subscriptionClient;
-            //_Logger = logger;
+            _EmailHandler = emailHandler;
+            _SubscriptionClient = subscriptionClient;
+            _Logger = logger;
         }
 
         protected override Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            //return Task.CompletedTask;
+            _SubscriptionClient.RegisterMessageHandler((message, token) =>
+            {
+                var invite = JsonConvert.DeserializeObject<Invitation>(Encoding.UTF8.GetString(message.Body));
+
+                _EmailHandler.GenerateAndSendInviteEmail(invite);
+
+                //Stops potential duplicate processing
+                return _SubscriptionClient.CompleteAsync(message.SystemProperties.LockToken);
+            },
+            new MessageHandlerOptions(args => Task.CompletedTask)
+            {
+                //exception handling
+                AutoComplete = false,
+                MaxConcurrentCalls = 1
+            });
             return Task.CompletedTask;
-            // _SubscriptionClient.RegisterMessageHandler((message, token) =>
-            // {
-            //     var invite = JsonConvert.DeserializeObject<Invitation>(Encoding.UTF8.GetString(message.Body));
-
-            //     //_EmailHandler.GenerateAndSendInviteEmail(invite);
-
-            //     //Stops potential duplicate processing
-            //     return _SubscriptionClient.CompleteAsync(message.SystemProperties.LockToken);
-            // },
-            // new MessageHandlerOptions(args => Task.CompletedTask)
-            // {
-            //     //exception handling
-            //     AutoComplete = false,
-            //     MaxConcurrentCalls = 1
-            // });
-            // return Task.CompletedTask;
         }
     }
 }
