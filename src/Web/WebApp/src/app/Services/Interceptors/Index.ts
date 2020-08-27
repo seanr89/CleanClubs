c71a245dad75ff2081aeea660cf1fa08d4dee485
seanr89 <srafferty89@gmail.com>
2020-08-27 20:51:59 +0100

    implemented the basics of an interceptor logic

---
+++ src/Web/WebApp/src/app/Services/Interceptors/Index.ts
@@ -0,0 +1,7 @@
+import { HTTP_INTERCEPTORS } from "@angular/common/http";
+import { APIInterceptor } from "./apiinterceptor";
+
+export const httpInterceptorProviders = [
+  { provide: HTTP_INTERCEPTORS, useClass: APIInterceptor, multi: true },
+  //{ provide: HTTP_INTERCEPTORS, useClass: NotifyInterceptor, multi: true }
+];
