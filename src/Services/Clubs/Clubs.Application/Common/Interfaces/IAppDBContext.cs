9d1fc5b922a67c46d7ab735b35ee9601161e8bf4
Sean <srafferty89@gmail.com>
2021-02-18 20:05:27 +0000

    interface addition for planning

---
+++ src/Services/Clubs/Clubs.Application/Common/Interfaces/IAppDBContext.cs
@@ -0,0 +1,10 @@
+namespace Clubs.Application.Common.Interfaces
+{
+    /// <summary>
+    /// Interface implementation to detail the logic implementation expected for a DBContext!
+    /// </summary>
+    public interface IAppDBContext
+    {
+
+    }
+}
